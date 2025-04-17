2bbd2440c2f317e10301fe0d11ac90f6fc47dd97
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2025-04-17 09:30:19 +0545

    banner dynamic

--- resources/views/frontend/about-us.blade.php
+++ resources/views/frontend/about-us.blade.php
@@ -2,26 +2,6 @@
 
 
 @section('content')
-    <section class="video_section mb-md-5 mb-4">
-        <!-- navbar -->
-        @include('frontend.layout.header')
-        @php
-            $defaultImage = asset('/frontend/images/about_banner.png'); // fallback image
-            $finalImage = $defaultImage;
-        @endphp
-        <div class="position-absolute top-0 start-0 w-100 h-100">
-            <div class="banner-container"
-                style="background-image: url({{ $finalImage }});background-size: cover;background-position: center;">
-                <div class="position-absolute top-0 start-0 w-100 h-100 z-1" style='background-color: #000000;opacity: 20%'>
-                </div>
-                <div class="banner-content">
-                    <h1 class="banner-title">About Us
-                    </h1>
-                </div>
-            </div>
-        </div>
-
-    </section>
 
  <!-- ultimate guide -->
  <section class="">
--- resources/views/frontend/inc/banner.blade.php
+++ resources/views/frontend/inc/banner.blade.php
@@ -7,16 +7,22 @@
             $banner = App\Models\MainSlider::where('link', request()->path())->first();
         @endphp
 
+        @if (str_starts_with($banner?->image->getMimeType(), 'image/'))
         <video loop muted autoPlay playsInline poster='./images/herobgvideo.mp4' class="video-banner">
-            <source src='{{asset('frontend/images/herobgvideo.mp4')}}' type="video/mp4" />
+            <source src='{{getImageUrl($banner->image)??asset('frontend/images/herobgvideo.mp4')}}' type="video/mp4" />
         </video>
         <div class="position-absolute top-0 start-0 w-100 h-100">
             <div class="banner-container">
                 <div class="position-absolute top-0 start-0 w-100 h-100 z-1"
                     style='background-color: #000000;opacity: 20%'></div>
                 <div class="banner-content">
-                    <p class="home_banner_subtitle mb-0">CELEBRATING 30 PLUS GLORIOUS YEARS OF SERVICE</p>
-                    <h1 class="home_banner_title mb-md-4">NEPAL</h1>
+                    @if ($banner->details)
+                    <p class="home_banner_subtitle mb-0">{{$banner->details}}</p>
+                    @endif
+                    @if ($banner->title)
+                    <h1 class="home_banner_title mb-md-4">{{$banner->title}}</h1>
+                    </h1>
+                    @endif
                     <div class="home_banner_input">
                         <div class="input-group search_input">
                             <span class="input-group-text bg-white ps-3 pe-0 rounded-start-2 py-md-3"
@@ -32,6 +38,29 @@
                 </div>
             </div>
         </div>
+        @else
+        @php
+            $defaultImage = asset('/frontend/images/about_banner.png'); // fallback image
+            $finalImage = getImageUrl($banner?->image)??$defaultImage;
+        @endphp
+        <div class="position-absolute top-0 start-0 w-100 h-100">
+            <div class="banner-container"
+                style="background-image: url({{ $finalImage }});background-size: cover;background-position: center;">
+                <div class="position-absolute top-0 start-0 w-100 h-100 z-1" style='background-color: #000000;opacity: 20%'>
+                </div>
+                <div class="banner-content">
+                    @if (isset($banner)&&$banner->details)
+                    <p class="home_banner_subtitle mb-0">{{$banner->details}}</p>
+                    @endif
+                    @if (isset($banner)&&$banner->title)
+                    <h1 class="home_banner_title mb-md-4">{{$banner->title}}</h1>
+                    </h1>
+                    @endif
+                </div>
+            </div>
+        </div>
+
+        @endif
     </div>
 
 </section>
